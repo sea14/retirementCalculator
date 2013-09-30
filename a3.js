59cc4ac72070d98079a878f913e4ab9b2286df35
sea14 <pianocat14@gmail.com>
2013-09-30 19:13:56 -0400

    changes to JS, html

--- a3.js
+++ a3.js
@@ -8,37 +8,47 @@ When the user clicks calculate, we run this function.*/
 
 function basicInput(){
 
-
+	var currentYear = new Date().getFullYear();
 	//check that birth year is positive and in the past
-	if(document.getElementById("").value>0 && ){	
+	if(document.getElementById("birthYear").value>0 && document.getElementById("birthYear").value < currentYear){	
 		this.birthYear = document.getElementById("birthYear").value;		
+		var greaterNum = currentYear - document.getElementById("birthYear").value;
+		return greaterNum;
 	}else{
-		document.write("Please provide a positive birth year that is in the past.");
+	
+		document.writeln("Please provide a positive birth year that is in the past.");
+		var greaterNum = 0;
 	}
 
 	//check that current savings are a number
-	if(document.getElementById("").value){
-		this.currentSavings = document.getElementById("current").value;
-	}else{
-		document.write("Please enter a numerical value for savings.");
-	}
+	//if(isNaN(document.getElementById("current").value)){
+	//	alert("Please enter a numerical value for savings.");
+	//}else{
+	//		 this.currentSavings = document.getElementById("current").value;
+	//}
 
 	//check that the expected retirement age is a number and is in the future
-	//we subtract the current year from the user's age of birth to get the number
+	//we subtract the current year from the user's year of birth to get the number
 	//that the user must have a retirement age greater than
 	
-	if(document.getElementById("").value){
+	var testRA = document.getElementById("expectedRA").value;
+	
+	//we calculate the greater number below
+
+	
+	var goodAge = greaterNum - testRA;
+	if(goodAge > testRA){
 		this.expectedRA = document.getElementById("expectedRA").value;
 	}else{
-		document.write("Please provide a numerical retirement age that is in the future.");
+		document.writeln("Please provide a numerical retirement age that is in the future.");
 	}
 
 	//check that life expectancy is a number and that it is in the future
 	//beyond the expected retirement age
-	if(document.getElementById("lifeExpectancy").value){
-		this.lifeExpectancy = document.getElementById("lifeExpectancy").value;
+	if(isNaN(document.getElementById("lifeExpectancy").value) || document.getElementById("lifeExpectancy").value - expectedRA){
+		document.writeln("Your life expectancy must be a number, in the future, and should be beyond the expected retirement age.");
 	}else{
-		document.write("Your life expectancy must be a number, in the future, and should be beyond the expected retirement age.");
+		this.lifeExpectancy = document.getElementById("lifeExpectancy").value;
 
 	}
 
@@ -51,7 +61,7 @@ function basicInput(){
 function scenarioInput(){
 	//check to see if we have a scenario name and it isn't composed of whitespace
 	if(document.getElementById("scenarioName").value==""){
-		document.write("Please enter a valid scenario name.");
+		document.writeln("Please enter a valid scenario name.");
 	}else{
 		this.scenarioName = document.getElementById("scenarioName").value;
 	}
@@ -59,13 +69,13 @@ function scenarioInput(){
 
 	//now we check the rates of return to make sure they're greater than 0
 	if(document.getElementById("work").value<=0){
-		document.write("Please enter a rate greater than 0.");
+		document.writeln("Please enter a rate greater than 0.");
 	}else{
 		this.workReturn = document.getElementById("work").value;
 	}
 	
-	if(document.getElementById("retire").value<=0){
-		document.write("Please enter a rate greater than 0.");
+	if(document.getElementById("retire").value <= 0){
+		document.writeln("Please enter a rate greater than 0.");
 	}else{
 		this.workReturn = document.getElementById("retire").value;
 
@@ -74,9 +84,13 @@ function scenarioInput(){
 
 	//now we check that the desired yearly income is a positive value
 	if(document.getElementById("yearly").value<=0){
-		document.write("Please enter a positive yearly income.");
+		document.writeln("Please enter a positive yearly income.");
 	}else{
 		this.yearlyIncome = document.getElementById("yearly").value;
 	}
 }
+
+//here's our fun little formula
+
+var savings = "blah blah blah";
 
